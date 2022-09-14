f3cae23d656aa919b5334400c5f93446b26ad15b
Julio <despairnatural@gmail.com>
2022-09-13 22:27:00 -0400

    Component BoxCenter

---
+++ src/components/Maps/BoxCenter.tsx
@@ -0,0 +1,9 @@
+import { styled } from "@mui/material";
+
+export const BoxCenter = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "48px",
+    height: "43px",
+  }));
